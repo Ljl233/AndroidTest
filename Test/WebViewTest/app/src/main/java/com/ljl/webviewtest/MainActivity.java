f7776d9f7aee82b15053798d63cd9c3b9827c871
Ljl233 <2955441566@qq.com>
2019-07-24 22:40:47 +0800

    web test

---
+++ Test/WebViewTest/app/src/main/java/com/ljl/webviewtest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ljl.webviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView=findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);//支持JavaScript脚本
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("www.baidu.com");
+    }
+}
