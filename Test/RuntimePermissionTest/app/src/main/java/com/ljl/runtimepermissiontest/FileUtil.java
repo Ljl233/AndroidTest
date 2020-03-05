84d332504a8e3790f9f7dda7db4bc9929fd3bcaf
Ljl233 <2955441566@qq.com>
2020-03-05 22:26:35 +0800

    从相册选择图片

---
+++ Test/RuntimePermissionTest/app/src/main/java/com/ljl/runtimepermissiontest/FileUtil.java
@@ -0,0 +1,9 @@
+package com.ljl.runtimepermissiontest;
+
+import android.net.Uri;
+
+public class FileUtil {
+    public static String getFilePathByUri(Uri uri) {
+        return null;
+    }
+}
---
+++ Test/RuntimePermissionTest/app/src/main/java/com/ljl/runtimepermissiontest/UpImageActivity.java
@@ -0,0 +1,149 @@
+package com.ljl.runtimepermissiontest;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+/**
+ * 1. 检查权限
+ * 2. 获取权限
+ * 3. 跳转相册，相机
+ * 4. 获取url
+ * 5. 根据url加载图片
+ */
+public class UpImageActivity extends AppCompatActivity {
+
+    private SimpleDraweeView mIvLoadImage;
+    private String[] mPermissions = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA};
+    private static final int REQUEST_CODE_READ = 1;
+    private static final int REQUEST_CODE_WRITE = 2;
+    private static final int REQUEST_CODE_CAMERA = 3;
+    private static final int CHOOSE_PHOTO = 4;
+    private static final int TAKE_PHOTO = 5;
+    private static final String TAG = "UpImageActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_up_image);
+
+        mIvLoadImage = findViewById(R.id.image_up);
+        mIvLoadImage.setOnClickListener(v -> {
+            if (checkAndRequestPermission()) {
+                Toast.makeText(this, "已授权。。", Toast.LENGTH_SHORT).show();
+                showPopWindow();
+            } else Toast.makeText(this, "请授权。。。", Toast.LENGTH_SHORT).show();
+//            checkAndRequestPermission();
+//            choosePhoto();
+        });
+    }
+
+    private void showPopWindow() {
+        View popView = LayoutInflater.from(this).inflate(R.layout.pop_photo_and_camera, null);
+        Button camera = popView.findViewById(R.id.pop_camera);
+        Button photo = popView.findViewById(R.id.pop_photo);
+        Button dismiss = popView.findViewById(R.id.pop_dismiss);
+        Log.e(TAG, "popView context" + popView.getContext().toString());
+        //获取手机的宽高
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels / 3;
+
+        final PopupWindow popupWindow = new PopupWindow(popView, width, height, true);
+        //点击外部 弹窗消失
+        popupWindow.setOutsideTouchable(true);
+        Log.e(TAG, "popview root" + popView.getRootView().toString());
+        popupWindow.showAtLocation(popView.getRootView(), Gravity.BOTTOM, 0, 0);
+        camera.setOnClickListener(v -> {
+            takePhoto();
+        });
+
+        photo.setOnClickListener(v -> {
+            popupWindow.dismiss();
+            openAlbum();
+        });
+    }
+
+    private void openAlbum() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        switch (requestCode) {
+            case CHOOSE_PHOTO:
+                Uri uri = data.getData();
+                mIvLoadImage.setImageURI(uri);
+                break;
+            case TAKE_PHOTO:
+                Uri uri2 = data.getData();
+                mIvLoadImage.setImageURI(uri2);
+                break;
+
+        }
+    }
+
+    //1. 检查权限
+    boolean hasPermission(String permission) {
+        return ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    //2. 获取权限
+    private boolean checkAndRequestPermission() {
+        boolean hasRead = false, hasWrite = false, hasCamera = false;
+        if (!hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE))
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_READ);
+        else hasRead = true;
+        if (!hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_WRITE);
+        else hasWrite = true;
+        if (!hasPermission(Manifest.permission.CAMERA))
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
+        else hasCamera = true;
+
+        return hasCamera && hasRead && hasWrite;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_READ:
+            case REQUEST_CODE_WRITE:
+            case REQUEST_CODE_CAMERA:
+        }
+    }
+}
